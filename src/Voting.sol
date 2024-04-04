48b54246999ed4bc0396225ba2df4ac7c013bbb2
corni <gmigan.a@gmail.com>
2024-04-05 00:09:08 +0200

    chore: add Openzepplin contracts to the project

--- src/Voting.sol
+++ src/Voting.sol
@@ -1,4 +1,15 @@
 // SPDX-License-Identifier: GPLv3
 pragma solidity ^0.8.13;
 
-contract Voting {}
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+// @title Voting contract
+// @author Antony Gandonou migna
+// @notice This contract is an exercice to learn about foundry and Dapp.
+// This contract allow to manage a voting process onchain.
+// @dev Administrator of voting sessions is the address which deployed
+// the contract. Admin can switch beteween deferents stats to manage
+// the voting process.
+contract Voting is Ownable {
+    constructor() Ownable(msg.sender) {}
+}
