48f1daa8b040c48b0579eb7f5190927e7fbd51a9
corni <gmigan.a@gmail.com>
2024-04-05 10:36:41 +0200

    test: test addVoter function

--- src/Voting.sol
+++ src/Voting.sol
@@ -11,5 +11,66 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 // the contract. Admin can switch beteween deferents stats to manage
 // the voting process.
 contract Voting is Ownable {
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint votedProposalId;
+    }
+
+    struct Proposal {
+        string description;
+        uint voteCount;
+    }
+
+    enum WorkflowStatus {
+        BeforeRegistrations,
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
+
+    uint256 winningProposalId;
+    WorkflowStatus public workflowStatus;
+    mapping(address => Voter) whitelist;
+
+    event VoterRegistered(address voterAddress);
+    event WorkflowStatusChange(
+        WorkflowStatus previousStatus,
+        WorkflowStatus newStatus
+    );
+    event ProposalRegistered(uint proposalId);
+    event Voted(address voter, uint proposalId);
+
     constructor() Ownable(msg.sender) {}
+
+    modifier onlyVoters() {
+        require(whitelist[msg.sender].isRegistered, "You're not a voter");
+        _;
+    }
+
+    // @return ID of the winner
+    function getWinner() external view returns (uint256) {
+        return winningProposalId;
+    }
+
+    // @dev allow the administrator to add a new address of voter to the
+    // whitelist.
+    // @dev Registration are available only if the contract is in
+    // the RegistrationVoters status.
+    function addVoter(address _voterAddress) external onlyOwner {
+        require(_voterAddress != address(0), "You can't add null address");
+        require(
+            workflowStatus == WorkflowStatus.RegisteringVoters,
+            "Voters registration is not open yet"
+        );
+        require(
+            whitelist[_voterAddress].isRegistered != true,
+            "Address already registered."
+        );
+        whitelist[_voterAddress].isRegistered = true;
+        emit VoterRegistered(_voterAddress);
+    }
 }
--- test/Voting.t.sol
+++ test/Voting.t.sol
@@ -1,6 +1,7 @@
+// SPDX-License-Identifier: GPLv3
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import "../src/Voting.sol";
 
 contract VotingTest is Test {
@@ -12,11 +13,22 @@ contract VotingTest is Test {
         votingInstance = new Voting();
     }
 
-    function testFail_Owner() public view {
+    function testFail_Owner() external view {
         assertEq(votingInstance.owner(), user1Address);
     }
 
-    function test_owner() public view {
+    function test_owner() external view {
         assertEq(votingInstance.owner(), owner);
+    }
+
+    function test_Revert_If_NullAddress_Is_Registered() external {
+        vm.expectRevert(bytes("You can't add null address"));
+        address nullAddress = address(0);
+        votingInstance.addVoter(nullAddress);
+    }
+
+    function test_Revert_If_Registration_Is_Not_Open() public {
+        vm.expectRevert(bytes("Voters registration is not open yet"));
+        votingInstance.addVoter(user1Address);
     }
 }
